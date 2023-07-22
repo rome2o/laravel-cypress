c979137ff9786e8f66a3f2db2229388112166902
Ali Kazim <ali@devkind.com.au>
2023-07-23 07:24:35 +1000

    fix: routes

--- src/routes/cypress.php
+++ src/routes/cypress.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Laracasts\Cypress\Controllers\CypressController;
 
-Route::post('/__cypress__/factory', [CypressController::class, 'factory'])->name('cypress.factory');
-Route::post('/__cypress__/logout', [CypressController::class, 'logout'])->name('cypress.logout');
-Route::post('/__cypress__/login/{userId}/{guard?}', [CypressController::class, 'login'])->name('cypress.login');
-Route::post('/__cypress__/artisan', [CypressController::class, 'artisan'])->name('cypress.artisan');
-Route::post('/__cypress__/run-php', [CypressController::class, 'runPhp'])->name('cypress.run-php');
-Route::get('/__cypress__/csrf_token', [CypressController::class, 'csrfToken'])->name('cypress.csrf-token');
-Route::post('/__cypress__/routes', [CypressController::class, 'routes'])->name('cypress.routes');
-Route::post('/__cypress__/current-user', [CypressController::class, 'currentUser'])->name('cypress.current-user');
+Route::post('/__cypress/factory', [CypressController::class, 'factory'])->name('cypress.factory');
+Route::post('/__cypress/logout', [CypressController::class, 'logout'])->name('cypress.logout');
+Route::post('/__cypress/login/{userId}/{guard?}', [CypressController::class, 'login'])->name('cypress.login');
+Route::post('/__cypress/artisan', [CypressController::class, 'artisan'])->name('cypress.artisan');
+Route::post('/__cypress/run-php', [CypressController::class, 'runPhp'])->name('cypress.run-php');
+Route::get('/__cypress/csrf_token', [CypressController::class, 'csrfToken'])->name('cypress.csrf-token');
+Route::post('/__cypress/routes', [CypressController::class, 'routes'])->name('cypress.routes');
+Route::post('/__cypress/current-user', [CypressController::class, 'currentUser'])->name('cypress.current-user');
